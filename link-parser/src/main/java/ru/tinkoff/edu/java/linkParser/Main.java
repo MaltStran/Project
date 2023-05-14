03755108e0ebab15047c186a6cdd8a6d05844473
MaltStran <niknik2430@mail.ru>
2023-05-15 00:17:47 +0500

    Task Solved

--- link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/linkParser/Main.java
@@ -1,22 +1,11 @@
 package ru.tinkoff.edu.java.linkParser;
 
-import java.net.MalformedURLException;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) throws MalformedURLException {
-
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите строку: ");
-        String url = in.nextLine();
-
-        LinkParser githubParser = new GithubParser();
-        LinkParser stackOverflowParser = new StackOverflowParser();
-        githubParser.setNextParser(stackOverflowParser);
-
-        String[] result = githubParser.parse(url);
-        System.out.println(Arrays.toString(result));
+    public static void main(String[] args){
+        ParserURL parserURL = new ParserURL();
+        System.out.println(parserURL.parse("https://github.com/sanyarnd/tinkoff-java-course-2022/"));
+        System.out.println(parserURL.parse("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
+        System.out.println(parserURL.parse("https://stackoverflow.com/search?q=unsupported%20link"));
+        System.out.println(parserURL.parse(null));
     }
 }
